236a332084f16bd210d5d6fac3e48b7b3b4c7d0f
mvmlabs <57446406+mvmlabs@users.noreply.github.com>
2022-12-15 22:33:08 +0100

    Delete Math.java

--- Exemples/UF2/Metode/src/cat/institutmvm/utils/Math.java
+++
@@ -1,8 +0,0 @@
-package cat.institutmvm.utils;
-
-public class Math {
-        //mètode que retorna la suma dels dos valors globals
-    public int sumar(int num1, int num2){
-        return num1+num2;
-    }
-}
